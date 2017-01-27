76a7e50350034eb6a9149f78c8d17ae04c7ea3d9
rallek <ralf@familie-koester.de>
2017-01-27 22:54:39 +0100

    initial upload of the team module

---
+++ generated modules/PggoTeamModule/Pggo/TeamModule/CHANGELOG.md
@@ -0,0 +1,3 @@
+# CHANGELOG
+
+Changes in PggoTeamModule 0.1.0
---
+++ generated modules/PggoTeamModule/Pggo/TeamModule/Resources/public/js/PggoTeamModule.js
@@ -0,0 +1,119 @@
+'use strict';
+
+function pggoTeamCapitaliseFirstLetter(string)
+{
+    return string.charAt(0).toUpperCase() + string.substring(1);
+}
+
+/**
+ * Initialise the quick navigation panel in list views.
+ */
+function pggoTeamInitQuickNavigation()
+{
+    var quickNavForm;
+    var objectType;
+
+    if (jQuery('.pggoteammodule-quicknav').length < 1) {
+        return;
+    }
+
+    quickNavForm = jQuery('.pggoteammodule-quicknav').first();
+    objectType = quickNavForm.attr('id').replace('pggoTeamModule', '').replace('QuickNavForm', '');
+
+    quickNavForm.find('select').change(function (event) {
+        quickNavForm.submit();
+    });
+
+    var fieldPrefix = 'pggoteammodule_' + objectType.toLowerCase() + 'quicknav_';
+    // we can hide the submit button if we have no visible quick search field
+    if (jQuery('#' + fieldPrefix + 'q').length < 1 || jQuery('#' + fieldPrefix + 'q').parent().parent().hasClass('hidden')) {
+        jQuery('#' + fieldPrefix + 'updateview').addClass('hidden');
+    }
+}
+
+/**
+ * Simulates a simple alert using bootstrap.
+ */
+function pggoTeamSimpleAlert(beforeElem, title, content, alertId, cssClass)
+{
+    var alertBox;
+
+    alertBox = ' \
+        <div id="' + alertId + '" class="alert alert-' + cssClass + ' fade"> \
+          <button type="button" class="close" data-dismiss="alert">&times;</button> \
+          <h4>' + title + '</h4> \
+          <p>' + content + '</p> \
+        </div>';
+
+    // insert alert before the given element
+    beforeElem.before(alertBox);
+
+    jQuery('#' + alertId).delay(200).addClass('in').fadeOut(4000, function () {
+        jQuery(this).remove();
+    });
+}
+
+/**
+ * Initialises the mass toggle functionality for admin view pages.
+ */
+function pggoTeamInitMassToggle()
+{
+    if (jQuery('.pggoteam-mass-toggle').length > 0) {
+        jQuery('.pggoteam-mass-toggle').click(function (event) {
+            jQuery('.pggoteam-toggle-checkbox').prop('checked', jQuery(this).prop('checked'));
+        });
+    }
+}
+
+/**
+ * Creates a dropdown menu for the item actions.
+ */
+function pggoTeamInitItemActions(context)
+{
+    var containerSelector;
+    var containers;
+    var listClasses;
+
+    containerSelector = '';
+    if (context == 'view') {
+        containerSelector = '.pggoteammodule-view';
+        listClasses = 'list-unstyled dropdown-menu dropdown-menu-right';
+    } else if (context == 'display') {
+        containerSelector = 'h2, h3';
+        listClasses = 'list-unstyled dropdown-menu';
+    }
+
+    if (containerSelector == '') {
+        return;
+    }
+
+    containers = jQuery(containerSelector);
+    if (containers.length < 1) {
+        return;
+    }
+
+    containers.find('.dropdown > ul').removeClass('list-inline').addClass(listClasses);
+    containers.find('.dropdown > ul a').each(function (index) {
+        jQuery(this).html(jQuery(this).html() + jQuery(this).find('i').first().data('original-title'));
+    });
+    containers.find('.dropdown > ul a i').addClass('fa-fw');
+    containers.find('.dropdown-toggle').removeClass('hidden').dropdown();
+}
+
+jQuery(document).ready(function() {
+    var isViewPage;
+    var isDisplayPage;
+
+    isViewPage = jQuery('.pggoteammodule-view').length > 0;
+    isDisplayPage = jQuery('.pggoteammodule-display').length > 0;
+
+    jQuery('a.lightbox').lightbox();
+
+    if (isViewPage) {
+        pggoTeamInitQuickNavigation();
+        pggoTeamInitMassToggle();
+        pggoTeamInitItemActions('view');
+    } else if (isDisplayPage) {
+        pggoTeamInitItemActions('display');
+    }
+});
