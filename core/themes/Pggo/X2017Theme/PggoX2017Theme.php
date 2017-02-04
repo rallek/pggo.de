01e9a2f9a366b71ee034d7923ebb3084e59b35f2
rallek <ralf@familie-koester.de>
2017-02-04 17:46:53 +0100

    initial upload theme X2017

---
+++ core/themes/Pggo/X2017Theme/PggoX2017Theme.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ *
+ * Copyright Pfarreiengemeinschaft Georgsmarienhuette Ost - http://pggo.de/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Pggo\X2017Theme;
+
+use Zikula\Bundle\CoreBundle\Bundle\AbstractCoreTheme;
+
+class PggoX2017Theme extends AbstractCoreTheme
+{
+}
